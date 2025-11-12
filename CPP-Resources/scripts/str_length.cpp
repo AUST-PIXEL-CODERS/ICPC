9aeb958cc26d3f813f1756026148c3b6ce246f7c
Daiyan-2004 <mdaiyan685@gmail.com>
2025-11-12 07:50:52 +0000

    Entry Script

---
+++ CPP-Resources/scripts/str_length.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char ch = 'A';
+  cout << "The length of the txt string is: " << txt.length() << "\n" << ch;
+  return 0;
+}
